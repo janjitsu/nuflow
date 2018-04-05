82fa204d4ac743627c8e9ae5ff259693c0cb86e1
Jan Silva <janfrs3@gmail.com>
2018-04-05 01:05:19 -0300

    Fetch all bill details

--- test_card_bills.py
+++ test_card_bills.py
@@ -8,16 +8,25 @@ from pprint import pprint
 from pynubank import Nubank
 from datetime import datetime
 
+credentials = json.load(open('nubank_credentials.json'))
+nu = Nubank(credentials['cpf'],credentials['password'])
 
 cache = CacheRequest('get_card_bills')
 
 if cache.has_cache():
     bills = cache.read_cache()
 else:
-    credentials = json.load(open('nubank_credentials.json'))
-    nu = Nubank(credentials['cpf'],credentials['password'])
     bills = nu.get_card_bills()
     cache.save_cache(bills);
 
 for bill in bills['bills']:
-   pprint(bill['state'])
+    if bill['state'] == 'overdue':
+
+        bill_cache = CacheRequest(bill['id'])
+
+        if bill_cache.has_cache():
+            bill_details = bill_cache.read_cache()
+        else:
+            bill_details = nu.get_card_bill_details(bill)
+            bill_cache.save_cache(bill_details);
+        pprint(bill_details)
