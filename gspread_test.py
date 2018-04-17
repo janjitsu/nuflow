af9a1b8021320f7b3ba7f223c20454ad37f5ab29
Jan Silva <janfrs3@gmail.com>
2018-04-17 00:42:59 -0300

    Add transactions to spreadsheet

--- gspread_test.py
+++ gspread_test.py
@@ -18,21 +18,36 @@ for sheet in sheets:
     if (len(sheets) > 1):
         spreadsheet.del_worksheet(sheet)
 
+
 add_next = False
 for bill in bills:
-    date = bill['bill']['summary']['open_date']
+    date = bill['bill']['summary']['close_date']
     name = datetime.strptime(date,'%Y-%m-%d').strftime('%Y%B')
-    pprint(bill['bill']['summary']['open_date'])
-    if (len(sheets) == 1):
-        sheet = spreadsheet.sheet1
-        sheet.title = name
-        sheet.sync()
-        add_next = True
+    pprint(bill['bill']['summary']['close_date'])
     if add_next:
         sheet = spreadsheet.add_worksheet(name)
+    if (len(sheets) == 1 and not add_next):
+        sheet = spreadsheet.sheet1
+        sheet.title = name
+        add_next = True
+
+    transaction_rows = []
+    for transaction in bill['bill']['transactions']:
+        transaction_date = datetime.strptime(
+            transaction['time'],
+            "%Y-%m-%dT%H:%M:%SZ"
+        ).strftime("%d/%m/%Y")
+        transaction_rows.append([
+            transaction_date,
+            transaction['description'],
+            transaction['title'],
+            ",".join(transaction['details']['tags']),
+            "%.2f" % (transaction['amount'] / 100)
+        ])
+    sheet.insert_rows(2,1,transaction_rows)
 
 
-#sys.exit(0)
+sys.exit(0)
 # Extract and print all of the values
 sheet.insert_rows(2,1,['25/02/2018','top sabor','restaurante','jan,karina'])
 list_of_hashes = sheet.get_all_records()
